5e92273f94a691545106befcc51b8560eaa158dc
JCrowson <jett.crowson@poly.com>
2022-06-29 11:04:34 -0600

    test

--- .barb/pre-push.py
+++ .barb/pre-push.py
@@ -1,26 +1,26 @@
+from importlib.machinery import SourceFileLoader
+from dotenv import load_dotenv
 import subprocess
 import sys
-from importlib.machinery import SourceFileLoader
-
-from dotenv import load_dotenv
 import os
 
 
 def _verify_version_change(v):
-    with open('./.barb/pre-push-cache.txt', 'r') as f:
+    with open('./.barb/version-cache', 'r') as f:
         return f.readline() != v
 
 
 def _version_cache_exists():
-    return os.path.exists('./.barb/pre-push-cache.txt')
+    return os.path.exists('./.barb/version-cache')
 
 
-def _create_version_cache(version):
+def _write_version_cache(version):
     with open('./.barb/pre-push-cache.txt', 'w') as f:
         f.write(version)
 
 
 def hook(*args):
+    print(args, os.environ.get('remote_ref'))
     load_dotenv()
 
     pypi_usr, pypi_pwd = os.environ.get('PYPI_USERNAME'), os.environ.get('PYPI_PASSWORD')
@@ -33,15 +33,16 @@ def hook(*args):
     module = SourceFileLoader("version", "./src/version.py").load_module()
     v = module.VERSION
     if not _version_cache_exists():
-        _create_version_cache(v)
+        _write_version_cache(v)
     else:
         print('Verifying package version number change.')
         if not _verify_version_change(v):
             print('Package version number has not been changed since last push. Cancelling.')
             sys.exit(1)
 
+        _write_version_cache(v)
+
     print("Attempting to build and upload...")
     subprocess.run(['bash', './publish.sh', pypi_usr, pypi_pwd])
 
     print("Successfully published the package.")
-
